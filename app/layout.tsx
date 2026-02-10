4861724c8caad67e366b3c872fb90a4389945cbf
coggiee <zentechie7@gmail.com>
2026-02-10 19:15:43 +0900

    chore: 404 페이지 제작

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} h-dvh antialiased`}>
         <ToastProvider>
           <AnchoredToastProvider>
             <TooltipProvider>{children}</TooltipProvider>
---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+  return (
+    <main className="flex h-full w-full flex-col items-center justify-center gap-8">
+      <header className="space-y-4 text-center">
+        <h2 className="font-bold text-2xl tracking-tighter md:text-3xl">Gone exploring… not found</h2>
+        <p className="text-sm md:text-base">Nothing so far!</p>
+      </header>
+      <aside className="flex flex-col items-center gap-2">
+        <Button variant={"link"} nativeButton={false} render={<Link href="/">Go Home</Link>} />
+      </aside>
+    </main>
+  );
+}
